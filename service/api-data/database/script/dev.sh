41c18db29390073305978613fa8b1ec04857a7fc
safizn <nassarsafi@gmail.com>
2025-03-26 05:06:51 +0200

    ci(script): minor dev script

--- service/api-data/database/script/dev.sh
+++ service/api-data/database/script/dev.sh
@@ -23,3 +23,7 @@ cluster#benchmark@api-data-database() {
     # TODO: benchmark CNPG cluster
     echo ''
 }
+
+load-sql.dev#example@api-data-database() {
+    psql -h localhost -p 5432 -U postgres -d app -f ./tmp/data.sql
+}
