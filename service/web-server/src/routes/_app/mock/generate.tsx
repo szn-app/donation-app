c823af3b00ad94dfdf999c5ceb6c36e774662d91
safizn <nassarsafi@gmail.com>
2025-05-29 16:24:01 +0300

    refactor: use autogenerated TS types rather than self defined interfaces

--- service/web-server/src/routes/_app/mock/generate.tsx
+++ service/web-server/src/routes/_app/mock/generate.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { MockGraphqlData } from "@/app/MockGraphqlData";
+import { MockGraphqlGenerate } from "@/app/MockGraphqlGenerate";
 
 export const Route = createFileRoute("/_app/mock/generate")({
-  component: MockGraphqlData,
-}); 
+  component: MockGraphqlGenerate,
+});
